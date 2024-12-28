df2e51f8409ddc13d0b83bc69e5b1fa755922031
Zhi Wei <ozwei51@gmail.com>
2024-12-28 15:44:43 +0800

    Update Library.java

--- Library.java
+++ Library.java
@@ -12,24 +12,12 @@ public class Library {
     }
 
     public void searchByTitle(String title) {
-        boolean found = false;
         for (Book book : books) {
-            // Directly access 'title' because it's public
             if (book.title.toLowerCase().contains(title.toLowerCase())) {
-                book.printBookInfo();
-                found = true;
-            }
-        }
-        if (!found) {
-            System.out.println("No books found with the title containing: " + title);
-        }
-    }
-
-    public void listAvailableBooks() {
-        for (Book book : books) {
-            if (book.isAvailable) {
-                book.printBookInfo();
+                System.out.println(book.title);  
             }
         }
     }
 }
+
+
